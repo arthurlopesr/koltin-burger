6091b5d178527b465309fa59a06cbce729c407df
Arthur Lopes <arthurlopr12@gmail.com>
2025-04-12 15:21:07 -0300

    first commit

---
+++ src/test/kotlin/org/example/kotlinburger/KotlinBurgerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlinburger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBurgerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
